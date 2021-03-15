574fc4236fb1297d3cafdf3279ea511a486e67a0
MokhtarElsmahy <67197450+MokhtarElsmahy@users.noreply.github.com>
2021-03-15 13:44:20 +0200

    re

---
+++ HaragApp/Data/Migrations/20210305143750_afterpull.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HaragApp.Data.Migrations
+{
+    public partial class afterpull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
